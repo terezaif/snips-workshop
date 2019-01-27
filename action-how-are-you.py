d48c56ce8b2a3d27aa0f8cb505926f99b4f5815c
Tereza Iofciu <t.iofciu@mytaxi.com>
2019-01-27 16:18:30 +0100

    added 2 intents

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -17,7 +17,8 @@ def main():
          .subscribe_intent(INTENT_BAD, feeling_bad_callback) \
          .start()
 
-jokes = ['How do you ask a tyrannosaur out to lunch? Tea, Rex?. Are you a dinosaur?', 
+jokes = ["What is a dinosaur's least favorite reindeer? Comet!",
+        'How do you ask a tyrannosaur out to lunch? Tea, Rex?. Are you a dinosaur?', 
 "What's the best way to talk to a velociraptor? Long distance!! Are you a dinosaur?", 
 "Why can't you hear a pterosaur using the bathroom? Because the p is silent! Are you a dinosaur?"]
 
