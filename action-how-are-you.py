3430c6150b5180f9187f59767030a1f6a6c16aa5
Tereza Iofciu <t.iofciu@mytaxi.com>
2019-01-27 16:05:22 +0100

    added 2 intents

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -18,7 +18,7 @@ def main():
          .start()
 
 jokes = ['How do you ask a tyrannosaur out to lunch? Tea, Rex?. Are you a dinosaur?', 
-"What’s the best way to talk to a velociraptor? Long distance!! Are you a dinosaur?", 
+"What's the best way to talk to a velociraptor? Long distance!! Are you a dinosaur?", 
 "Why can't you hear a pterosaur using the bathroom? Because the p is silent! Are you a dinosaur?"]
 
 
