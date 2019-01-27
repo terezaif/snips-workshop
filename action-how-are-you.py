63804330485926a915816c12bd3058c2446291ce
Tereza Iofciu <t.iofciu@mytaxi.com>
2019-01-27 14:44:17 +0100

    added trex

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -7,8 +7,7 @@ INTENT_HOW_ARE_YOU = "terezaif:how_are_you"
 
 def main():
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
-         .start()
+        h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback).start()
 
 
 def how_are_you_callback(hermes, intent_message):
